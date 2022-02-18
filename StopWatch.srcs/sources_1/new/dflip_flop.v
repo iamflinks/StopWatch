34bf59cbfa35a6c64cd11b0f932ee45cd2bebe5c
Femi Olugbon <olugbonfemi@gmail.com>
2022-02-18 15:55:15 -0500

    made changes to q_bar

--- StopWatch.srcs/sources_1/new/dflip_flop.v
+++ StopWatch.srcs/sources_1/new/dflip_flop.v
@@ -48,21 +48,3 @@ module dflip_flop(   D,              // Data input
         end   
 
 endmodule
-
-//// FPGA projects using Verilog/ VHDL 
-//// fpga4student.com
-//// Verilog code for D Flip FLop
-//// Verilog code for Rising edge D flip flop with Asynchronous Reset high
-//module RisingEdge_DFlipFlop_AsyncResetHigh(D,clk,async_reset,Q);
-//input D; // Data input 
-//input clk; // clock input 
-//input async_reset; // asynchronous reset high level
-//output reg Q; // output Q 
-//always @(posedge clk or posedge async_reset) 
-//begin
-// if(async_reset==1'b1)
-//  Q <= 1'b0; 
-// else 
-//  Q <= D; 
-//end 
-//endmodule
