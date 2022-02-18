2216adeafe8e1ba5f22b6901c323f341bbc02e23
Femi Olugbon <olugbonfemi@gmail.com>
2022-02-18 09:30:37 -0500

    added seven segment display decoder

---
+++ StopWatch.srcs/sources_1/new/dispControl.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/18/2022 09:29:02 AM
+// Design Name: 
+// Module Name: dispControl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dispControl(
+
+    );
+endmodule
