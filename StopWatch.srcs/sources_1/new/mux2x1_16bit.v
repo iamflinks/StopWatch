2206046fcabe4d7ce97d71e904dca2591a30d183
Femi Olugbon <olugbonfemi@gmail.com>
2022-02-18 08:13:46 -0500

    First commit: added mutltiplexer design

---
+++ StopWatch.srcs/sources_1/new/mux2x1_16bit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/18/2022 07:59:15 AM
+// Design Name: 
+// Module Name: mux2x1_16bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2x1_16bit(
+
+    );
+endmodule
