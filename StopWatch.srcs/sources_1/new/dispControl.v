0683bf7bfe6633c6b439e3ae1ca1df6496852ca0
Femi Olugbon <olugbonfemi@gmail.com>
2022-02-18 10:16:39 -0500

    made changes to the display control code

--- StopWatch.srcs/sources_1/new/dispControl.v
+++ StopWatch.srcs/sources_1/new/dispControl.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: University of New Hampshire
+// Engineer: Femi Olugbon
 // 
-// Create Date: 02/18/2022 09:29:02 AM
-// Design Name: 
+// Create Date: 02/18/2022 08:35:46 AM
+// Design Name: StopWatch
 // Module Name: dispControl
-// Project Name: 
-// Target Devices: 
+// Project Name: StopWathc Design
+// Target Devices: Xilinx Artix-7 FPGA
 // Tool Versions: 
 // Description: 
 // 
-// Dependencies: 
+// Dependencies: None
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -20,7 +20,34 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module dispControl(
+module dispControl(  input0,
+                     input1,
+                     input2,
+                     input3,
+                     output0,
+                     output1,
+                     output2,
+                     output3,
+                     output4,
+                     output5,
+                     output6,
+                     );
 
-    );
+    input   input0, input1, input2, input3;
+    output  output0, output1, output2, output3, output4, output5, output6;
+    
+    assign output0 = input3 | (~input1 & input2) | (~input0 & input1) | (input1 & ~input2);
+    
+    assign output1 = (~input0 & ~input1 & ~input2) | (~input1 & input2 & ~input3) | (~input0 & input1 & input2) | (input0 & ~input2 & input3) | (input0 & input1 & input3);
+    
+    assign output2 = (~input0 & ~input2) | (~input1 & ~input2 & input3) | (~input3 & input1) | (input1 & input2) | (input0 & input2 & ~input3);
+    
+    assign output3 = (~input0 & ~input1 & ~input3) | (~input0 & ~input2) | (~input1 & ~input2) | (input0 & ~input1 & input3) | (input0 & input1 & ~input3);
+    
+    assign output4 = (~input1 | ~input2) | (~input2 & input3) | (input2 & ~input3) | (input0 & ~input1) | (input0 & ~input2);
+    
+    assign output5 = (~input0 & ~input2) | (~input1 & input3) | (~input0 & input1) | (input1 & ~input2) | (input0 & ~input1 & input2);
+    
+    assign output6 = (~input0 & ~input2) | (input2 & input3) | (~input0 & input1) | (input1 & input3);
+
 endmodule
