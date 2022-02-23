735907009bdae95d9b843df69e6e0ab319c2c560
Femi Olugbon <olugbonfemi@gmail.com>
2022-02-23 16:56:01 -0500

    change implementation

--- StopWatch.srcs/sources_1/new/dispControl.v
+++ StopWatch.srcs/sources_1/new/dispControl.v
@@ -20,34 +20,54 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module dispControl(  input0,
-                     input1,
-                     input2,
-                     input3,
-                     output0,
-                     output1,
-                     output2,
-                     output3,
-                     output4,
-                     output5,
-                     output6,
+module dispControl(  inputNumber,
+                     segmentPin,
+                     controlPin
                      );
 
-    input   input0, input1, input2, input3;
-    output  output0, output1, output2, output3, output4, output5, output6;
+    input  [15:0] inputNumber;
+    output reg [8:0] segmentPin;
+    output reg [3:0] controlPin;
     
-    assign output0 = input3 | (~input1 & input2) | (~input0 & input1) | (input1 & ~input2);
+    //Anode activating signals
+    always @ (*) 
+        begin
+//            case
+        end
     
-    assign output1 = (~input0 & ~input1 & ~input2) | (~input1 & input2 & ~input3) | (~input0 & input1 & input2) | (input0 & ~input2 & input3) | (input0 & input1 & input3);
+    // BCD to seven segment decoder, common anode
+    always @(*)
+        begin
+            case(ledBCD)
+                4'b0000: ledOut = 7'b0000001; // "0"     
+                4'b0001: ledOut = 7'b1001111; // "1" 
+                4'b0010: ledOut = 7'b0010010; // "2" 
+                4'b0011: ledOut = 7'b0000110; // "3" 
+                4'b0100: ledOut = 7'b1001100; // "4" 
+                4'b0101: ledOut = 7'b0100100; // "5" 
+                4'b0110: ledOut = 7'b0100000; // "6" 
+                4'b0111: ledOut = 7'b0001111; // "7" 
+                4'b1000: ledOut = 7'b0000000; // "8"     
+                4'b1001: ledOut = 7'b0000100; // "9" 
+                default: ledOut = 7'b0000001; // "0"
+            endcase
+        end
     
-    assign output2 = (~input0 & ~input2) | (~input1 & ~input2 & input3) | (~input3 & input1) | (input1 & input2) | (input0 & input2 & ~input3);
+//    input   input0, input1, input2, input3;
+//    output  output0, output1, output2, output3, output4, output5, output6;
     
-    assign output3 = (~input0 & ~input1 & ~input3) | (~input0 & ~input2) | (~input1 & ~input2) | (input0 & ~input1 & input3) | (input0 & input1 & ~input3);
+//    assign output0 = input3 | (~input1 & input2) | (~input0 & input1) | (input1 & ~input2);
     
-    assign output4 = (~input1 | ~input2) | (~input2 & input3) | (input2 & ~input3) | (input0 & ~input1) | (input0 & ~input2);
+//    assign output1 = (~input0 & ~input1 & ~input2) | (~input1 & input2 & ~input3) | (~input0 & input1 & input2) | (input0 & ~input2 & input3) | (input0 & input1 & input3);
     
-    assign output5 = (~input0 & ~input2) | (~input1 & input3) | (~input0 & input1) | (input1 & ~input2) | (input0 & ~input1 & input2);
+//    assign output2 = (~input0 & ~input2) | (~input1 & ~input2 & input3) | (~input3 & input1) | (input1 & input2) | (input0 & input2 & ~input3);
     
-    assign output6 = (~input0 & ~input2) | (input2 & input3) | (~input0 & input1) | (input1 & input3);
+//    assign output3 = (~input0 & ~input1 & ~input3) | (~input0 & ~input2) | (~input1 & ~input2) | (input0 & ~input1 & input3) | (input0 & input1 & ~input3);
+    
+//    assign output4 = (~input1 | ~input2) | (~input2 & input3) | (input2 & ~input3) | (input0 & ~input1) | (input0 & ~input2);
+    
+//    assign output5 = (~input0 & ~input2) | (~input1 & input3) | (~input0 & input1) | (input1 & ~input2) | (input0 & ~input1 & input2);
+    
+//    assign output6 = (~input0 & ~input2) | (input2 & input3) | (~input0 & input1) | (input1 & input3);
 
 endmodule
